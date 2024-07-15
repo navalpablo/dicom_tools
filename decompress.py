63e1ee5eacc414ea89176a4842280dd41da58246
navalpablo <93452498+navalpablo@users.noreply.github.com>
2024-07-15 09:36:35 +0200

    parallel process

--- decompress.py
+++ decompress.py
@@ -3,6 +3,7 @@ import subprocess
 import argparse
 from tqdm import tqdm
 from shutil import which
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 def is_tool_available(name):
     """Check if tool is available in PATH and executable."""
@@ -12,10 +13,10 @@ def decompress_file(file_path):
     try:
         # Attempt to decompress the file, overwriting the original
         subprocess.run(["dcmdjpeg", file_path, file_path], check=True)
-        return True  # Return True if successful
+        return file_path, True  # Return True if successful
     except subprocess.CalledProcessError:
         # This error is raised for non-DICOM files or other failures
-        return False  # Return False on failure
+        return file_path, False  # Return False on failure
 
 def walk_and_process(directory):
     # Collect all files to process to calculate the progress
@@ -25,12 +26,18 @@ def walk_and_process(directory):
             file_paths.append(os.path.join(root, file))
 
     # Initialize the progress bar
-    progress_bar = tqdm(file_paths, desc="Decompressing files", unit="file")
-    for file_path in progress_bar:
-        result = decompress_file(file_path)
-        # Update the description on the progress bar
-        progress_bar.set_description(f"Processing {os.path.basename(file_path)}")
-        progress_bar.set_postfix(success=result)
+    progress_bar = tqdm(total=len(file_paths), desc="Decompressing files", unit="file")
+    
+    # Use ThreadPoolExecutor for parallel processing
+    with ThreadPoolExecutor() as executor:
+        futures = {executor.submit(decompress_file, file_path): file_path for file_path in file_paths}
+        
+        for future in as_completed(futures):
+            file_path, result = future.result()
+            # Update the description on the progress bar
+            progress_bar.set_description(f"Processing {os.path.basename(file_path)}")
+            progress_bar.set_postfix(success=result)
+            progress_bar.update(1)
 
 def main():
     parser = argparse.ArgumentParser(description='Recursively decompress DICOM files in a directory tree.')
